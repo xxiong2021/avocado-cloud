579314936b06dbc56f728082c74ac23e650feba4
xxiong2021 <89631515+xxiong2021@users.noreply.github.com>
2025-05-16 13:34:23 +0800

    Update test_functional_azure-vm-utils.py

--- tests/azure/test_functional_azure-vm-utils.py
+++ tests/azure/test_functional_azure-vm-utils.py
@@ -54,8 +54,6 @@ class Azure_vm_utilsTest(Test):
     def setUp(self):
         self.cloud = Setup(self.params, self.name)
         self.vm = self.cloud.vm  # Access the VM created during setup
-        #publicip = AzurePublicIP(self.params, name=self.vm.vm_name)
-        self.log.info("publicip: %s",publicip)
         publicip_name = self.vm.vm_name + "publicip"
         publicip = AzurePublicIP(self.params,name=publicip_name)
         self.log.info("publicip: %s",publicip)
