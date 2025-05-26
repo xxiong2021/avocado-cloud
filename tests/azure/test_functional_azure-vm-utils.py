325db5b55308c21a0b9b64bf14a51e80e6195adc
xxiong2021 <89631515+xxiong2021@users.noreply.github.com>
2025-05-26 10:22:19 +0800

    Update test_functional_azure-vm-utils.py

--- tests/azure/test_functional_azure-vm-utils.py
+++ tests/azure/test_functional_azure-vm-utils.py
@@ -31,6 +31,34 @@ class Azure_vm_utilsTest(Test):
         self.vm = self.cloud.vm  # Access the VM created during setup
         authentication = "publickey"
         self.session = self.cloud.init_vm(authentication=authentication)
+        # if self.vm.exists():
+        #     self.vm.delete()
+        file_path = '/root/azure-vm-utils/result.txt'
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+        key = "/root/.ssh/id_rsa.pub"
+        self.vm.ssh_key_value = "{}".format(key)
+        self.vm.authentication_type = "ssh"
+        self.vm.vm_name += "-utils"
+        # self.vm.os_disk_name += "-new"
+
+        osdisk = self.vm.properties["storageProfile"]["osDisk"]["vhd"]["uri"]
+        self.vm.delete()
+        self.vm.image = osdisk
+        #self.vm.vm_username = origin_username
+        self.vm.os_disk_name += "-new"
+
+        self.vm.create(wait=True)
+        self.session.connect(authentication="publickey")
+        self.assertEqual(self.vm.vm_username,
+                         self.session.cmd_output("whoami"),
+                         "Fail to login with publickey")
+        self.assertIn(
+            "%s ALL=(ALL) NOPASSWD:ALL" % self.vm.vm_username,
+            self.session.cmd_output(
+                "sudo cat /etc/sudoers.d/90-cloud-init-users"),
+            "No sudo privilege")
 
     def test_selftest_without_imds_symlink_validation(self):
         """
@@ -39,34 +67,7 @@ class Azure_vm_utilsTest(Test):
         try:
             #publicip = AzurePublicIP(self.params, name=self.vm.vm_name)
             #self.log.info("publicip: %s",publicip)
-            # if self.vm.exists():
-            #     self.vm.delete()
-            file_path = '/root/azure-vm-utils/result.txt'
-            if os.path.exists(file_path):
-                os.remove(file_path)
-    
-            key = "/root/.ssh/id_rsa.pub"
-            self.vm.ssh_key_value = "{}".format(key)
-            self.vm.authentication_type = "ssh"
-            self.vm.vm_name += "-utils"
-            # self.vm.os_disk_name += "-new"
-
-            osdisk = self.vm.properties["storageProfile"]["osDisk"]["vhd"]["uri"]
-            self.vm.delete()
-            self.vm.image = osdisk
-            #self.vm.vm_username = origin_username
-            self.vm.os_disk_name += "-new"
-
-            self.vm.create(wait=True)
-            self.session.connect(authentication="publickey")
-            self.assertEqual(self.vm.vm_username,
-                             self.session.cmd_output("whoami"),
-                             "Fail to login with publickey")
-            self.assertIn(
-                "%s ALL=(ALL) NOPASSWD:ALL" % self.vm.vm_username,
-                self.session.cmd_output(
-                    "sudo cat /etc/sudoers.d/90-cloud-init-users"),
-                "No sudo privilege")
+           
             
             publicip_name = self.vm.vm_name + "PublicIP"
             cmd = ' az network public-ip show   --name {} --resource-group "{}"  --query "ipAddress"   --output tsv'.format(publicip_name, self.vm.resource_group)
